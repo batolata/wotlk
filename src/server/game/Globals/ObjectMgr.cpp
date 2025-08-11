d9ae13ee95208e0358d957b63f8f4a54f9113cb1
DecrypteD <batolata1337@gmail.com>
2025-08-11 16:03:59 +0800

    Update ObjectMgr.cpp

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -217,7 +217,7 @@ bool normalizePlayerName(std::string& name)
     if (!Utf8toWStr(name, tmp))
         return false;
 
-    wstrToLower(tmp);
+    //wstrToLower(tmp);
     if (!tmp.empty())
         tmp[0] = wcharToUpper(tmp[0]);
 
